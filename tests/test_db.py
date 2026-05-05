b42759df8a76f84a1265bc5d71b69b325e90a9ac
Jono Sachs <jonosachs@gmail.com>
2026-05-05 13:25:56 +1000

    Test_add - Include pytest single run command reminder

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,6 +3,7 @@ from services.db import Declined
 from tests.mock_event import mock_event
 
 
+# Run single test: python -m pytest tests/test_db.py -s
 def test_add():
     d = Declined()
     d.add(event=mock_event)
